5e7dd3954d2d3782f2f66378056496c28beae785
SKISKIDAISKI\jxhnsilver <infedinwork@gmail.com>
2025-08-03 22:29:25 +0700

    Initial commit

---
+++ src/eShopApplicationOnWeb.WebAPI/Program.cs
@@ -0,0 +1,37 @@
+
+namespace eShopApplicationOnWeb.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/openapi/v1.json", "OpenAPI V1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
