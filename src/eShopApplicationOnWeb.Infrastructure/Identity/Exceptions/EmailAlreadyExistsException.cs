8747ee01ec6f9ed7cac5d850d9a9085e475adb30
SKISKIDAISKI\jxhnsilver <infedinwork@gmail.com>
2025-08-09 00:31:34 +0700

    Add identity custom exceptions

---
+++ src/eShopApplicationOnWeb.Infrastructure/Identity/Exceptions/EmailAlreadyExistsException.cs
@@ -0,0 +1,14 @@
+﻿using eShopApplicationOnWeb.Infrastructure.Identity.Exceptions.Common;
+using Microsoft.AspNetCore.Http;
+
+namespace eShopApplicationOnWeb.Infrastructure.Identity.Exceptions
+{
+    public class EmailAlreadyExistsException : IdentityException
+    {
+        public override int StatusCode => StatusCodes.Status409Conflict;
+        public override string ErrorType => "email_already_exists";
+
+        public EmailAlreadyExistsException(string email)
+            : base($"User with email '{email}' already exists") { }
+    }
+}
