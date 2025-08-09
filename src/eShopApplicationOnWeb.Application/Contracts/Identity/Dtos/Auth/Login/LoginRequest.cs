983868bd469dc007ab1d455df8f0d27683dfde9e
SKISKIDAISKI\jxhnsilver <infedinwork@gmail.com>
2025-08-09 20:15:28 +0700

    Add auth login request/response dtos

---
+++ src/eShopApplicationOnWeb.Application/Contracts/Identity/Dtos/Auth/Login/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eShopApplicationOnWeb.Application.Contracts.Identity.Dtos.Auth.Login
+{
+    public class LoginRequest
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
