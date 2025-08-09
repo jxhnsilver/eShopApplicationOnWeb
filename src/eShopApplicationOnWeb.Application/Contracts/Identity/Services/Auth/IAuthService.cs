bbd86e9f3159072e90cecaa9371516aea722390e
SKISKIDAISKI\jxhnsilver <infedinwork@gmail.com>
2025-08-09 20:27:48 +0700

    Implement authentication login with JWT token

--- src/eShopApplicationOnWeb.Application/Contracts/Identity/Services/Auth/IAuthService.cs
+++ src/eShopApplicationOnWeb.Application/Contracts/Identity/Services/Auth/IAuthService.cs
@@ -1,9 +1,11 @@
-﻿using eShopApplicationOnWeb.Application.Contracts.Identity.Dtos.Auth.Register;
+﻿using eShopApplicationOnWeb.Application.Contracts.Identity.Dtos.Auth.Login;
+using eShopApplicationOnWeb.Application.Contracts.Identity.Dtos.Auth.Register;
 
 namespace eShopApplicationOnWeb.Application.Contracts.Identity.Services.Auth
 {
     public interface IAuthService
     {
         Task<RegisterResponse> RegisterAsync(RegisterRequest request);
+        Task<LoginResponse> LoginAsync(LoginRequest request);
     }
 }
--- src/eShopApplicationOnWeb.Infrastructure/Identity/Services/Auth/AuthService.cs
+++ src/eShopApplicationOnWeb.Infrastructure/Identity/Services/Auth/AuthService.cs
@@ -1,7 +1,9 @@
-﻿using eShopApplicationOnWeb.Application.Contracts.Identity.Dtos.Auth.Register;
+﻿using eShopApplicationOnWeb.Application.Contracts.Identity.Dtos.Auth.Login;
+using eShopApplicationOnWeb.Application.Contracts.Identity.Dtos.Auth.Register;
 using eShopApplicationOnWeb.Application.Contracts.Identity.Services.Auth;
 using eShopApplicationOnWeb.Infrastructure.Identity.Exceptions;
-using eShopApplicationOnWeb.Infrastructure.Identity.Persistence.Models;
+using eShopApplicationOnWeb.Infrastructure.Identity.Models;
+using eShopApplicationOnWeb.Infrastructure.Identity.Security.Tokens.Jwt;
 using Microsoft.AspNetCore.Identity;
 
 namespace eShopApplicationOnWeb.Infrastructure.Identity.Services.Auth
@@ -9,10 +11,36 @@ namespace eShopApplicationOnWeb.Infrastructure.Identity.Services.Auth
     public class AuthService : IAuthService
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        public AuthService(UserManager<ApplicationUser> userManager)
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        public AuthService(UserManager<ApplicationUser> userManager, IJwtTokenGenerator jwtTokenGenerator)
         {
             _userManager = userManager;
+            _jwtTokenGenerator = jwtTokenGenerator;
         }
+
+        public async Task<LoginResponse> LoginAsync(LoginRequest request)
+        {
+            var applicationUser = await _userManager.FindByEmailAsync(request.Email);
+            if (applicationUser == null)
+                throw new InvalidCredentialsException();
+
+            var isPasswordValid = await _userManager.CheckPasswordAsync(applicationUser, request.Password);
+            if (!isPasswordValid)
+                throw new InvalidCredentialsException();
+
+            var roles = await _userManager.GetRolesAsync(applicationUser);
+
+            var token = _jwtTokenGenerator.GenerateToken(applicationUser, roles);
+
+            var response = new LoginResponse
+            {
+                UserId = applicationUser.Id,
+                Token = token
+            };
+
+            return response;
+        }
+
         public async Task<RegisterResponse> RegisterAsync(RegisterRequest request)
         {
             if (await _userManager.FindByEmailAsync(request.Email) is not null)
@@ -27,13 +55,11 @@ namespace eShopApplicationOnWeb.Infrastructure.Identity.Services.Auth
                 Email = request.Email
             };
 
-            var createUserResult = await _userManager.CreateAsync(applicationUser);
-
+            var createUserResult = await _userManager.CreateAsync(applicationUser, request.Password);
             if (!createUserResult.Succeeded)
                 throw new IdentityOperationFailedException(createUserResult.Errors);
 
             var addRoleResult = await _userManager.AddToRoleAsync(applicationUser, "User");
-
             if (!addRoleResult.Succeeded)
                 throw new IdentityOperationFailedException(createUserResult.Errors);
 
