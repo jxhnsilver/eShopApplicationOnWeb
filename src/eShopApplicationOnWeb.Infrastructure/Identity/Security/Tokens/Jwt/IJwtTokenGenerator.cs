208f188483fb07c0f94743d89f3886f6a7ccda0c
SKISKIDAISKI\jxhnsilver <infedinwork@gmail.com>
2025-08-09 20:24:51 +0700

    Add JWT token generation logic for user authentication

---
+++ src/eShopApplicationOnWeb.Infrastructure/Identity/Security/Tokens/Jwt/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using eShopApplicationOnWeb.Infrastructure.Identity.Models;
+
+namespace eShopApplicationOnWeb.Infrastructure.Identity.Security.Tokens.Jwt
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser, IList<string> roles);
+    }
+}
