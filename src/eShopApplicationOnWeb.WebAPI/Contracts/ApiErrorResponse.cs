a41854faf40c05191155cd6b3de162ca9f34b013
SKISKIDAISKI\jxhnsilver <infedinwork@gmail.com>
2025-08-10 20:49:46 +0700

    Add api error response dto

---
+++ src/eShopApplicationOnWeb.WebAPI/Contracts/ApiErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace eShopApplicationOnWeb.WebAPI.Contracts
+{
+    public class ApiErrorResponse
+    {
+        public string Type { get; set; }
+        public string Title { get; set; }
+        public int StatusCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
